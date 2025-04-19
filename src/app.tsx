18090778221b016b781e08c0249c76d7fd6db1fc
NoahClemmensen <noah0940@edu.sde.dk>
2025-04-19 17:25:36 +0200

    Basic features

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,12 @@ import {SettingsSection} from "spcr-settings";
 import './resources/styles.css';
 
 const settings = new SettingsSection("Nyan-Bar-Synced Settings", "nyanbarsynced-settings");
+const nyanTargetSelector = ".progress-bar__slider"; // Selector for the nyan bar element
 
 let audioData: any = null; // Variable to hold audio data
 
 // Function to wait for a specific DOM element to appear before proceeding
-async function waitForElement(selector: any, maxAttempts = 50, interval = 100) {
+async function waitForElement(selector: any, maxAttempts = 50, interval = 100): Promise<HTMLElement> {
     let attempts = 0;
     while (attempts < maxAttempts) {
         const element = document.querySelector(selector); // Attempt to find the element
@@ -28,14 +29,53 @@ async function fetchAudioData(retryDelay = 200, maxRetries = 10) {
             const message: any = error.message;
 
             if (message.includes("Cannot read properties of undefined") && maxRetries > 0) {
-                console.log("[CAT-JAM-PARTY] Retrying to fetch audio data...");
+                console.log("[NYAN-BAR-SYNCED] Retrying to fetch audio data...");
                 await new Promise(resolve => setTimeout(resolve, retryDelay));
                 return fetchAudioData(retryDelay, maxRetries - 1); // Retry fetching audio data
             }
         } else {
-            console.warn(`[CAT-JAM-PARTY] Error fetching audio data: ${error}`);
+            console.warn(`[NYAN-BAR-SYNCED] Error fetching audio data: ${error}`);
         }
         return null; // Return default playback rate on failure
+    }
+}
+
+// Function to sync the nyan bar with the audio data via the Spicetify API and css variables
+async function syncNyan(audioData: any) {
+    let tempo = 116; // Default tempo in BPM
+    let MPB: number;
+
+    try {
+        if (!audioData) {
+            audioData = await fetchAudioData(); // Fetch audio data if not available
+        }
+        tempo = audioData.track.tempo;
+    } catch (error) {
+        console.error("[NYAN-BAR-SYNCED] Error fetching audio data:", error);
+    } finally {
+        MPB = 60_000 / tempo; // Convert BPM to MPB (Milliseconds per Beat)
+    }
+
+    try {
+        const nyanBar = await waitForElement(nyanTargetSelector);
+        if (!nyanBar) return;
+
+        nyanBar.setAttribute("style", "--anim-speed: " + MPB + "ms;")
+        nyanBar.classList.remove("play-anim");
+        nyanBar.classList.add("play-anim");
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function playPauseNyan() {
+    const nyanBar = await waitForElement(nyanTargetSelector);
+    if (!nyanBar) return;
+
+    if (Spicetify.Player.isPlaying()) {
+        await syncNyan(audioData);
+    } else {
+        nyanBar.classList.remove("play-anim");
     }
 }
 
@@ -58,17 +98,17 @@ async function main() {
     // Create settings UI for the extension
     createSettingsUI();
 
+    // Initialize the nyan bar animation
+    if (Spicetify.Player.isPlaying()) await syncNyan(audioData);
+
     // Add event listeners for player state changes
     Spicetify.Player.addEventListener("onplaypause", async () => {
-
-    });
-
-    Spicetify.Player.addEventListener("onprogress", async () => {
-
+        await playPauseNyan();
     });
 
     Spicetify.Player.addEventListener("songchange", async () => {
         audioData = await fetchAudioData(); // Fetch new audio data on song change
+        await syncNyan(audioData);
     });
 }
 
